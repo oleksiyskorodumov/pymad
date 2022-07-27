fcf57d52e8b3f42bb54787cf419855e681da668d
Oleksiy Skorodumov <oleksiy.skorodumov@miarec.com>
2022-07-27 23:38:08 +0300

    try fix error: invalid command 'bdist_wheel'

--- setup.py
+++ setup.py
@@ -2,12 +2,10 @@
 
 """Setup script for the MAD module distribution."""
 
-import os
-import re
 import sys
-
-from distutils.core import setup
-from distutils.extension import Extension
+import setuptools
+import distutils.core
+import os.path
 
 VERSION_MAJOR = 0
 VERSION_MINOR = 10
@@ -18,7 +16,7 @@ DEFINES = [('VERSION_MAJOR', VERSION_MAJOR),
            ('VERSION_MINOR', VERSION_MINOR),
            ('VERSION', '"%s"' % PYMAD_VERSION)]
 
-MADMODULE = Extension(
+MADMODULE = distutils.core.Extension(
     name='mad',
     sources=['src/madmodule.c', 'src/pymadfile.c', 'src/xing.c'],
     define_macros=DEFINES,
@@ -26,7 +24,7 @@ MADMODULE = Extension(
     library_dirs=['libmad/.libs'],
     libraries=['mad'])
 
-setup(  # Distribution metadata
+setuptools.setup(  # Distribution metadata
     name='pymad',
     version=PYMAD_VERSION,
     description='A wrapper for the MAD libraries.',
