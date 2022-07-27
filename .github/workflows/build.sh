372aff4db9b17e0a48f6c4293795ea26f8229908
Oleksiy Skorodumov <oleksiy.skorodumov@miarec.com>
2022-07-27 23:18:15 +0300

    try fix error: invalid command 'bdist_wheel'

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -19,10 +19,9 @@ make
 # setup .whl
 cd ..
 for PYBIN in /opt/python/*/bin; do
-    if [[ $PYBIN = *"cp27"* ]]; then
+    if [[ $PYBIN = *"cp27"* || $PYBIN = *"cp35"* || $PYBIN = *"cp36"* ]]; then
         continue
     fi
-    "${PYBIN}/python" -m pip install --user wheel
     "${PYBIN}/python" setup.py bdist_wheel -d dist
 done
 for whl in dist/*.whl; do
